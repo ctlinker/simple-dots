07873877968f376923f1488ae299e3960101ec31
ctlinker <166874035+ctlinker@users.noreply.github.com>
2026-05-01 00:13:22 +0000

    feat(fish): added fish config
    - env "proto language manager from moon"
    - env "cargo environement"
    - xdg dir var loading
    - custom fish greeting
    - ssh-agent, starfish autoload

---
+++ dotfiles/.config/fish/config.fish
@@ -0,0 +1,32 @@
+# Commands to run in interactive sessions can go here
+if status is-interactive
+    # Starship prompt
+    starship init fish | source
+    # Aliases
+    alias ls='ls --color=auto'
+    alias grep='grep --color=auto'
+end
+
+# SSH Agent
+if not set -q SSH_AUTH_SOCK
+    eval (ssh-agent -c)
+    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+end
+
+# Custom fish greeting
+function fish_greeting
+    echo "[Welcome to Fish! $(whoami)] Type 'help' to see the Fish shell documentation"
+    echo ""
+    fastfetch
+end
+
+# Environment variables
+set -gx PROTO_HOME "$HOME/.proto"
+set -gx PATH "$PROTO_HOME/shims" "$PROTO_HOME/bin" $PATH
+
+# Cargo env
+eval "sh $HOME/.cargo/env"
+
+# XDG Directories
+cat "$HOME/.config/user-dirs.dirs" | sed -E '/^#/d; s/^(XDG_[^=]+)="?([^"]+)"?/set -gx \1 \2/' | source
